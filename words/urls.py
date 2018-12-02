89b7983a97b1d6bc2b262cb072f869109722f088
Lobby A <lobbya@hhtowers.local>
2018-12-02 16:11:11 +0200

    added a search bar with post

--- words/urls.py
+++ words/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.index,name="words-index"),
+    path('words/',views.index,name="words-index"),
     path('words/<str:word_name>/', views.detail, name='words-detail'),
 ]
--- words/views.py
+++ words/views.py
@@ -3,15 +3,16 @@ from django.http import HttpResponse, Http404
 from django.template import loader
 from .models import Word
 from django.shortcuts import redirect, reverse
+from django.http import HttpResponseRedirect
 # Create your views here.
 
 def index(request):
-    # if request.method == 'GET': # If the form is submitted
-    #     search_query = request.GET.get('search_box', None)
-    #     return redirect(reverse('words/') + search_query)
-
-    word_list = Word.objects.all()
-    return render(request, 'words/index.html',{'word_list': word_list})
+    if request.method == 'POST': # If the form is submitted
+         search_query = request.POST.get('query', None)
+         return HttpResponseRedirect('/words/'+ str(search_query))
+    else:
+        word_list = Word.objects.all()
+        return render(request, 'words/index.html',{'word_list': word_list})
 
 def detail(request,word_name):
     word = get_object_or_404(Word, word_name=word_name)
