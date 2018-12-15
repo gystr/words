f6f4230bf5996cf316802ff3512f63dcea94a94c
Guy <38810886+GoldenTimeLover@users.noreply.github.com>
2018-12-15 19:16:16 +0200

    added a nonfunctional form to add words

--- words/urls.py
+++ words/urls.py
@@ -3,7 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('',views.index,name="words-index"),
     path('words/',views.index,name="words-index"),
     path('random/',views.get_random,name="random"),
     path('words/<str:word_name>/', views.detail, name='words-detail'),
+    path('words/add/<str:word_name>', views.add_word, name="words-add_word_details"),
+    # path('add/',views.add,name="words-add_action"),
 ]
--- words/views.py
+++ words/views.py
@@ -10,16 +10,25 @@ import random
 # Create your views here.
 
 def index(request):
-    if request.method == 'POST': # If the form is submitted
-         search_query = request.POST.get('query', None)
-         return HttpResponseRedirect('/words/'+ str(search_query))
-    else:
+    if request.method == 'POST': # If the form is submitted with POST
+         search_query = request.POST.get('query', None) # Get query from form
+         return HttpResponseRedirect('/words/'+ str(search_query)) # Return redirect to .detail
+    else: # Else display regular index page
         word_list = Word.objects.all()
         return render(request, 'words/index.html',{'word_list': word_list})
 
+# def detail(request,word_name):
+#     word = get_object_or_404(Word, word_name=word_name)
+#     return render(request, 'words/detail.html', {'word': word})
+
 def detail(request,word_name):
-    word = get_object_or_404(Word, word_name=word_name)
-    return render(request, 'words/detail.html', {'word': word})
+    try: #Try to find the word that was provided in the URl
+        word = Word.objects.get(word_name=word_name)
+        return render(request, 'words/detail.html', {'word': word})
+    except Word.DoesNotExist:#Except error and redirect to /words/add/+query
+        return HttpResponseRedirect('/words/add/'+ str(word_name))
+    except:# If another error occurs Return404 ¯\_(ツ)_/¯
+        return Http404("An Error Has Occured At Detail Page!")
 
 def get_random(request):
         max_id = Word.objects.all().aggregate(max_id=Max("id"))['max_id']
@@ -28,3 +37,8 @@ def get_random(request):
             category = Word.objects.filter(pk=pk).first()
             if category:
                 return HttpResponseRedirect('/words/'+ str(category.word_name))
+
+def add_word(request,word_name):
+    return render(request, 'words/addWord.html',{'word_name': word_name})
+
+# def add(request):
