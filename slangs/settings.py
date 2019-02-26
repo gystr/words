b52f09ec60247e7734141aea5c2922d487b1f714
Guy <38810886+GoldenTimeLover@users.noreply.github.com>
2019-02-26 20:11:25 +0200

    removed users

--- slangs/settings.py
+++ slangs/settings.py
@@ -6,7 +6,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
- 
+
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'zk5zw)jonn-8zgyd8fkjj1*l!uqmu_pp!&1ta87_#mprb1e&-$'
 
@@ -20,7 +20,6 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'words.apps.WordsConfig',
-    'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
